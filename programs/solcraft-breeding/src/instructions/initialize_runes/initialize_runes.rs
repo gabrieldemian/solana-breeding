ad4878c25c4836d280bbefe4124df02e69d8d4be
Gabriel Costa <gabriel@pixelvault.com>
2022-05-17 00:12:16 -0300

    minting tokensssssss

--- programs/solcraft-breeding/src/instructions/initialize_runes/initialize_runes.rs
+++ programs/solcraft-breeding/src/instructions/initialize_runes/initialize_runes.rs
@@ -5,29 +5,29 @@ use crate::state::PREFIX_PIG;
 use super::InitializeRunes;
 
 pub fn handler(ctx: Context<InitializeRunes>, _bump: u8, _rune: String) -> Result<()> {
-  // let _mint = &ctx.accounts.mint;
+  let mint = &ctx.accounts.mint.to_account_info();
+  let token = &ctx.accounts.token.to_account_info();
   let pig_machine = &ctx.accounts.pig_machine;
-  let _payer = &ctx.accounts.payer;
+  let payer = &ctx.accounts.payer.to_account_info();
   let _rent = &ctx.accounts.rent;
 
-  let _signers_seeds = [
+  let signers_seeds = [
       PREFIX_PIG.as_bytes(),
       &[pig_machine.bump]
   ];
 
-  // anchor_spl::token::initialize_mint(
-  //   CpiContext::new_with_signer(
-  //     ctx.accounts.token_program.to_account_info(),
-  //       anchor_spl::token::InitializeMint{
-  //         mint: mint.to_account_info().clone(),
-  //         rent: rent.to_account_info().clone(),
-  //         },
-  //         &[&signers_seeds]
-  //       ),
-  //       8,
-  //       &pig_machine.key(),
-  //       Some(&pig_machine.key()),
-  // )?;
+  anchor_spl::token::mint_to(
+    CpiContext::new_with_signer(
+        ctx.accounts.token_program.to_account_info(),
+        anchor_spl::token::MintTo{
+            mint: mint.clone(),
+            to: token.clone(),
+            authority: payer.clone()
+        },
+        &[&signers_seeds],
+    ),
+    100000000
+  )?;
 
   Ok(())
 }
